58b0c3c828fadf04d613e36b963499e1313165e1
TjD <konijn@gmail.com>
2013-03-09 17:11:53 -0500

    Update PP.js

--- PP.js
+++ PP.js
@@ -1,2 +1,26 @@
 //One global to rule them all
-PP = {};
+PP = {
+
+  width: 80,
+  height: 50,
+
+  newGame : function(n)
+  {
+    PP.game = {};
+    PP.game.description = 'Save game ' + (n++)
+    PP.game.seed = Math.floor(Math.random()*100000000);
+    PP.game.level = 1;
+    PP.save.store();
+  },
+
+  playGame : function
+  {
+    PP.display.clear();  
+    ROT.RNG.setSeed( PP.game.seed );
+    PP.map = new ROT.Map.Uniform( PP.width , PP.height , { roomDugPercentage : 0.95  } );
+    PP.map.create(PP.display.DEBUG);
+    PP.rooms = map.getRooms();
+            
+  }
+  
+}
