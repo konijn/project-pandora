42618456c3ab922cd88f086a677b9b216e6ee33d
TjD <konijn@gmail.com>
2013-03-09 17:18:41 -0500

    Update saveload.js

--- saveload.js
+++ saveload.js
@@ -1,24 +1,40 @@
 //`Namespace`
-PP.save = {};
-//Create save game list
-PP.save.list = function()
-{
-  var count  = 5;
-  var list = [];
-  for( var i = 0 ; i < count ; i++ )
+PP.save = {
+  //Create save game list
+  list : function()
   {
-    var s = localStorage["save"+i]
-    if(!s)
+    var count  = 5;
+    var list = [];
+    for( var i = 0 ; i < count ; i++ )
     {
-      list.push( (i+1) + ") New Game")
+      var s = localStorage[ PP.save.key( i ) ];
+      if(!s)
+      {
+        list.push( (i+1) + ") New Game")
+      }
+      else
+      {
+        list.push( (i+1) + ") " + JSON.parse( s ).description )
+      }
     }
-    else
-    {
-      list.push( (i+1) + ") " + JSON.parse( s ).description )
-    }
-  }
   return list;
+  }
+  //Store the game in progress,
+  //Should be done when the game starts and when a new level is reached
+  //Highly save scummable in other words
+  store : function()
+  {
+    s = JSON.stringify( PP.game );
+    localStorage[ PP.save.key( PP.game.n -  1 ) ] = s;  
+  }
+
+  key : function( n )
+  {
+    return "save" + n; 
+  }
+
 }
+
 //Load a game, or start a game
 //This is expected to be called by a key handler, by the way ;)
 PP.load = function( code , char , vk )
@@ -31,11 +47,14 @@ PP.load = function( code , char , vk )
   if( !( n > 0 && n < 6  ) )
     return;
   //Get it
-  var s = localStorage["save"+(n-1)]
+  var s = localStorage[ PP.save.key( n - 1 ) ];
   if(!s)
-    PP.newGame();
+  {
+    return PP.newGame();
+  }
   else
-    console.log( "No loading possible at this point" );
+  {
+    PP.game = JSON.parse( s );
+    PP.playGame();
+  }
 }
-
-
