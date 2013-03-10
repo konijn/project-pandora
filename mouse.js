e039a0899f91c31f35f6f72bb57fb8292b6f5a05
TjD <konijn@gmail.com>
2013-03-10 18:20:08 -0300

    Create mouse.js

---
+++ mouse.js
@@ -0,0 +1,27 @@
+
+
+PP.mouse = {
+
+  listening : false,
+
+  listen : function()
+  {
+    if(!PP.mouse.listening)
+    {
+      document.getElementsByTagName("canvas")[0].addEventListener("click", function(e)
+      {
+        var x = Math.floor(  ( e.x - this.offsetLeft ) /  (  this.width / PP.width  )  );
+        var y = Math.floor(  ( e.y - this.offsetTop ) /  (  this.height / PP.height  )  );
+        if( PP.mouse.next )
+        {
+          PP.mouse.next( x , y , e );
+        }
+      }
+      PP.mouse.listening = true;
+    }
+  }
+
+}
+
+
+
