dda6414be64421e991fa1550167842999094eb2d
TjD <konijn@gmail.com>
2013-03-10 18:21:06 -0300

    Update keyboard.js

--- keyboard.js
+++ keyboard.js
@@ -1,23 +1,26 @@
-PP.kb = { listening : false }; //Keyboard
+PP.kb = { 
+  
+  listening : false,
 
-PP.kb.listen = function()
-{
-  if( !PP.kb.listening )
+  listen : function()
   {
-    document.addEventListener("keydown", function(e) 
+    if( !PP.kb.listening )
     {
-      var code = e.keyCode;
-      var vk = "?"; /* find the corresponding constant */
-      for (var name in ROT) 
+      document.addEventListener("keydown", function(e) 
       {
-        if (ROT[name] == code && name.indexOf("VK_") == 0) { vk = name; }
-      }
-      console.log( "Keydown: code is " + code + "(  " + String.fromCharCode(code) + "  ) , (" + vk + ")" );
-      if( PP.kb.next )
-      {
-        PP.kb.next( code , String.fromCharCode(code) , vk );
-      }
-    });
-    PP.kb.listening = true;
+        var code = e.keyCode;
+        var vk = "?"; /* find the corresponding constant */
+        for (var name in ROT) 
+        {
+          if (ROT[name] == code && name.indexOf("VK_") == 0) { vk = name; }
+        }
+        console.log( "Keydown: code is " + code + "(  " + String.fromCharCode(code) + "  ) , (" + vk + ")" );
+        if( PP.kb.next )
+        {
+          PP.kb.next( code , String.fromCharCode(code) , vk );
+        }
+      });
+      PP.kb.listening = true;
+    }
   }
 }
